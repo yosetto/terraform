bcf1bb3b4da19a3406411248189205888c3fa547
Youssef <ettolbayos@gmail.com>
2022-05-12 14:21:51 +0200

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+   value = ["${module.vpc.vpc_id}"]
+}
+
+output "vpc_public_subnets" {
+   value = ["${module.vpc.public_subnets}"]
+}
+
+output "webserver_ids" {
+   value = ["${aws_instance.webserver.*.id}"]
+}
+
+output "ip_addresses" {
+   value = ["${aws_instance.webserver.*.id}"]
+}
