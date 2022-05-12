66da7e743ff6412d557ab5c1b667d1137bb61a24
Youssef <ettolbayos@gmail.com>
2022-05-12 12:15:55 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,7 @@
-
+provider "aws" {
+   access_key = var.aws_access_key
+   secret_key = var.aws_secret_key
+   token = var.aws_api_token
+   region = var.aws_region
+   skip_credentials_validation = true
+}
